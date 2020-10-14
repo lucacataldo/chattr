4aba60432789db40bab253913e5ba4eaac4fa474
luca <luca9716@gmail.com>
2020-10-13 21:16:05 -0400

    dotenv and now ws on same port

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app)
+const io = require("socket.io").listen(server);
+require("dotenv").config();
 
 app.use(express.static("./src/frontend"));
 
-const io = require("socket.io")();
 io.on("connection", (client) => {
 	console.log("Connected");
 	client.on("msg", (data) => {
 		console.log(client);
 		console.log(data);
-		client.broadcast.emit("newMsg", {from: "other", message: data});
+		client.broadcast.emit("newMsg", { from: "other", message: data });
 	});
 });
 
-io.listen(6968);
-
-app.listen(6969, () => {
+server.listen(process.env.PORT, () => {
 	console.log("Listening");
 });
